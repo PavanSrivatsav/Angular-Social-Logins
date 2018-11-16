cd7e906f1206c2af73607b5a7296a62550a53863
PavanSrivatsav <pavansrivatsav96@gmail.com>
2018-11-17 00:36:36 +0530

    initial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,8 @@
 import { Component } from '@angular/core';
+import {
+  AuthService,
+  GoogleLoginProvider
+} from 'angular-6-social-login';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +10,31 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+
+  constructor(private socialAuthService: AuthService) {
+    }
+
+    public socialSignIn(socialPlatform: string) {
+      console.log("inside");
+      let socialPlatformProvider;
+      if (socialPlatform === 'google') {
+         socialPlatformProvider = GoogleLoginProvider.PROVIDER_ID;
+      }
+      
+      this.socialAuthService.signIn(socialPlatformProvider).then(userData => {
+         this.apiConnection(userData);
+      });
+      }
+
+      apiConnection(data) {
+       console.log("ConnectionData",data);
+     }
+
+     logout() {
+      this.socialAuthService.signOut().then(data => {
+        console.log("logout->",data)
+   });
+  }
+
   title = 'HRMS';
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,37 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 
+import {
+  SocialLoginModule,
+  AuthServiceConfig,
+  GoogleLoginProvider
+} from 'angular-6-social-login';
+
+
+export function getAuthServiceConfigs() {
+  const config = new AuthServiceConfig(
+  [
+  {
+      id: GoogleLoginProvider.PROVIDER_ID,
+      provider: new GoogleLoginProvider('347965750066-o0pnh4a47nirp79od5gugcjc4j18mfu4.apps.googleusercontent.com')
+  }
+  ]
+  );
+  return config;
+  }
+
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    SocialLoginModule
   ],
-  providers: [],
+  providers: [{
+    provide: AuthServiceConfig,
+    useFactory: getAuthServiceConfigs
+    }],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
